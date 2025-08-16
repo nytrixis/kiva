6504fe584d5618a1177bc577064fb20ec0209578
Nandini Pandey <ajayapandey2404@gmail.com>
2025-08-16 17:29:34 +0530

    mail updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -138,8 +138,8 @@ export default function Footer() {
               </li>
               <li className="flex items-center">
                 <Mail className="mr-2 h-5 w-5 text-primary" />
-                <a href="mailto:info@kiva.com" className="text-gray-600 hover:text-primary transition-colors">
-                  info@mykiva.in
+                <a href="mailto:contact@ajyaa.in" className="text-gray-600 hover:text-primary transition-colors">
+                  contact@ajyaa.in
                 </a>
               </li>
             </ul>
